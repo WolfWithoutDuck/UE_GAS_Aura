60baab40c6c28c3224b75d352a7a281afaede7ed
shanxiee <shanxiee@outlook.com>
2024-11-04 09:05:14 +0800

    完善经验作为元属性添加，以及属性点添加

--- Source/aura/Public/Player/AuraPlayerState.h
+++ Source/aura/Public/Player/AuraPlayerState.h
@@ -7,8 +7,12 @@
 #include "GameFramework/PlayerState.h"
 #include "AuraPlayerState.generated.h"
 
+class ULevelUpInfo;
 class UAttributeSet;
 class UAbilitySystemComponent;
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChanged, int32/*StatValue*/)
+
 /**
  * 
  */
@@ -23,7 +27,34 @@ public:
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
+	UFUNCTION(BlueprintCallable)
 	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetXP() const { return XP; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetAttributePoints() const { return AttributePoints; }
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetSpellPoints() const { return SpellPoints; }
+
+	//升级经验信息
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<ULevelUpInfo> LevelUpInfo;
+
+	FOnPlayerStatChanged OnXPChangedDelegate;
+	FOnPlayerStatChanged OnLevelChangedDelegate;
+	FOnPlayerStatChanged OnAttributePointsChangedDelegate;
+	FOnPlayerStatChanged OnSpellPointsChangedDelegate;
+
+	void AddToXP(int32 InXP);
+	void AddToLevel(int32 InLevel);
+	void AddToAttributePoints(int32 InAttributePoints);
+	void AddToSpellPoints(int32 InSpellPoints);
+
+	void SetXP(int32 InXP);
+	void SetLevel(int32 InLevel);
 
 protected:
 	UPROPERTY(VisibleAnywhere)
@@ -36,6 +67,24 @@ private:
 	UPROPERTY(VisibleAnywhere, ReplicatedUsing= OnRep_Level)
 	int32 Level = 1;
 
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing= OnRep_XP)
+	int32 XP = 1;
+
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing= OnRep_AttributePoints)
+	int32 AttributePoints = 0;
+
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing= OnRep_SpellPoints)
+	int32 SpellPoints = 1;
+
 	UFUNCTION()
 	void OnRep_Level(int32 OldLevel);
+
+	UFUNCTION()
+	void OnRep_XP(int32 OldXP);
+
+	UFUNCTION()
+	void OnRep_AttributePoints(int32 OldAttributePoints);
+
+	UFUNCTION()
+	void OnRep_SpellPoints(int32 OldSpellPoints);
 };
